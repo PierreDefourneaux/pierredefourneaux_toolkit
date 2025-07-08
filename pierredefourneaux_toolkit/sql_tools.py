4aba7625fb2a616ab73707f46ffba6d73ac97a73
Pierre Defourneaux <pierre.defourneaux@gmail.com>
2025-07-08 19:58:34 +0200

    amélioration de la fonction create_table_and_insert_rows_from_csv

--- pierredefourneaux_toolkit/sql_tools.py
+++ pierredefourneaux_toolkit/sql_tools.py
@@ -32,8 +32,7 @@ class SqlManager:
         try:
             mysql_password = os.getenv("MYSQL_PASSWORD")
             if not mysql_password:
-                raise ValueError("Le mot de passe MySQL \"MYSQL_PASSWORD\" n'a pas été trouvé dans les variables d'environnement." \
-                "Il faut avoir, dans un fichier .env dans le même dossier, une variable nommée 'MYSQL_PASSWORD' stockant le mot de passe MySQL.")
+                raise ValueError("La variable d'environnement \"MYSQL_PASSWORD\" n'a pas été trouvée dans le .env du dossier courant.")
             engine = create_engine(f"mysql+pymysql://{mysql_user}:{mysql_password}@{host}/{database}")
             query = text(query)
 
@@ -67,8 +66,7 @@ class SqlManager:
         try:
             mysql_password = os.getenv("MYSQL_PASSWORD")
             if not mysql_password:
-                raise ValueError("Le mot de passe MySQL \"MYSQL_PASSWORD\" n'a pas été trouvé dans les variables d'environnement." \
-                "Il faut avoir, dans un fichier .env dans le même dossier, une variable nommée 'MYSQL_PASSWORD' stockant le mot de passe MySQL.")
+                raise ValueError("La variable d'environnement \"MYSQL_PASSWORD\" n'a pas été trouvée dans le .env du dossier courant.")
 
             engine = create_engine(f"mysql+pymysql://{mysql_user}:{mysql_password}@{host}/{database}")
             dropper = text(f"drop table if exists {table}")
@@ -86,7 +84,10 @@ class SqlManager:
         types:list,
         mysql_user:str,
         host:str,
-        database:str
+        database:str,
+        add_auto_id: bool = True,
+        primary_keys: list = None,
+        foreign_keys: dict = None
         ):
         """Créé une table et insère les données depuis un fichier csv. Le nom de la table sera celui du csv.
         Il faut avoir, dans un fichier .env dans le même dossier, une variable nommée 'MYSQL_PASSWORD' stockant le mot de passe MySQL.
@@ -97,7 +98,9 @@ class SqlManager:
             mysql_user (str): utilisateur MySQL.
             host (str): host de la base de données. Localhost, adresse IP ou nom de domaine.
             database (str): la base de données MySQL sur laquelle on souhaite opérer.
-
+            add_auto_id (bool): rajoute un id auto-incrémenté du nom de la table. Annulé si un argument primary_keys est déclaré.
+            primary_keys (list): liste de la / des colonnes constitutives de la clé primaire de la table. Ecrase add_auto_id.
+            foreign_keys (dict): dictionnaire des clés étrangères reçue par la table. Exemple : {"client_id": ("clients", "id"), "produit_id": ("produits", "id")}
         Raises:
             ValueError: Retourne l'erreur de nombre de colonnes dans la liste de types demandés.
             ValueError: Retourne l'erreur de type invalide dans la liste de types demandés.
@@ -105,15 +108,17 @@ class SqlManager:
         Prints:
             print(): messages de confirmation de création de la table, des colonnes puis d'insertion des lignes"""
         try:
+            # Définir le nom de la table
             nom_de_la_table = re.sub(".csv","",csv)
             df = pd.read_csv(csv)
-
             colonnes = list(df.columns)
+
+            # Vérifier la bonne correspondance nombre de types demandés/nombre de colonnes
             if len(colonnes) != len(types):
-                raise ValueError("Le nombre de types souhaités pour les colonnes de la table n'est pas égal aux nombre de colonnes dans le CSV." \
-                f"({len(types)} type(s) demandé(s) dans la liste pour {len(colonnes)} colonne(s)) ")
+                raise ValueError(f"Le nombre de types demandés ({len(types)}) ne correspond pas au nombres de colonnes du CSV ({len(colonnes)}).")
+            
+            # Vérifier la conformité des types demandés pour Mysql. Pour rappel, booléens = TINYINT
             colonnes_types = list(zip(colonnes, types))
-            #vérifier les types
             valid_types = (
                 "TINYINT", "SMALLINT", "MEDIUMINT", "INT", "INTEGER", "BIGINT",
                 "DECIMAL", "NUMERIC", "FLOAT", "DOUBLE", "DATE", "DATETIME",
@@ -121,48 +126,63 @@ class SqlManager:
                 "TINYTEXT", "MEDIUMTEXT", "LONGTEXT", "BLOB", "ENUM", "JSON"
             )
             for col, t in colonnes_types:
-                base_type = t.split("(")[0].upper()  # gère par ex VARCHAR(255)
+                base_type = t.split("(")[0].upper()  # gère par exemple varchar(255)
                 if base_type not in valid_types:
                     raise ValueError(
                         f"Type invalide pour la colonne `{col}` : `{t}` n'est pas un type MySQL reconnu. Types MySQL reconnus par la méthode :{valid_types}"
                     )
+            # Connexion à MySQL
             mysql_password = os.getenv("MYSQL_PASSWORD")
             if not mysql_password:
-                raise ValueError("Le mot de passe MySQL \"MYSQL_PASSWORD\" n'a pas été trouvé dans les variables d'environnement." \
-                "Il faut avoir, dans un fichier .env dans le même dossier, une variable nommée 'MYSQL_PASSWORD' stockant le mot de passe MySQL.")
+                raise ValueError("La variable d'environnement \"MYSQL_PASSWORD\" n'a pas été trouvée dans le .env du dossier courant.")
             engine = create_engine(f"mysql+pymysql://{mysql_user}:{mysql_password}@{host}/{database}")
-            sql1 = f"""
-            CREATE TABLE IF NOT EXISTS {nom_de_la_table}(
-                id INT AUTO_INCREMENT PRIMARY KEY)
-            """
+
+            # Initialisation de la requête de création de la table
+            sql_create_table = f"CREATE TABLE IF NOT EXISTS {nom_de_la_table} (\n"
+            colonnes_a_creer = []
+
+            # Append optionnel de la colonne table_id auto-incrémenté
+            if add_auto_id and not primary_keys:
+                colonnes_a_creer.append(f"{nom_de_la_table}_id INT AUTO_INCREMENT PRIMARY KEY")
+
+            # Append des colonnes normales du csv
+            for col, typ in zip(colonnes, types):
+                col_def = f"`{col}` {typ}"
+                colonnes_a_creer.append(col_def)
+
+            # Rajouter à la fin de la requete la liste des colonnes constitutives de la clé primaire de la table
+            if primary_keys:
+                pk_str = ", ".join([f"`{k}`" for k in primary_keys])
+                colonnes_a_creer.append(f"PRIMARY KEY ({pk_str})")
+
+            # Rajouter à la fin de la requete le dictionnaire des clés étrangères reçues par la table
+            if foreign_keys:
+                for col, (ref_table, ref_col) in foreign_keys.items():
+                    colonnes_a_creer.append(f"FOREIGN KEY (`{col}`) REFERENCES `{ref_table}`(`{ref_col}`)")
+
+            # Finalisation et exécution
+            sql_create_table += ",\n".join(colonnes_a_creer) + "\n);"
             with engine.connect() as conn:
-                conn.execute(text(sql1))
-                print(f"table {nom_de_la_table} créée")
-            for col_tuple in colonnes_types :
-                sql2 = f"""
-                    ALTER TABLE {nom_de_la_table} ADD COLUMN {col_tuple[0]} {col_tuple[1]}
-                    """
-                with engine.connect() as conn:
-                    conn.execute(text(sql2))
-                    print(f"colonne {col_tuple[0]} créée pour des types {col_tuple[1]}")
-            # la syntaxe de base pour INSERT INTO est 
-            # INSERT INTO nom_de_la_table (colonne1, colonne2, colonne3, ...)
-            # VALUES (valeur1, valeur2, valeur3, ...);
-            # Avec SQLAlchemy on peut utiliser des placeholders, cela évite les injections SQL et gère bien les types.
-            # INSERT INTO clients (nom, age, ville) => On déclare les colonnes
-            # VALUES (:nom, :age, :ville); => on pose les :placeholders → sûr contre les injections
-            # {"nom": "Alice", "age": 28, "ville": "Nice"} puis on passe un dictionnaire
+                conn.execute(text(sql_create_table))
+                print(f"Table `{nom_de_la_table}` créée avec succès.")
+
+            # Insertion des lignes du csv
             for _, row in df.iterrows():
                 colonnes_str = ", ".join([f"`{col}`" for col in colonnes])
-                placeholder = ", ".join([f":{col}" for col in colonnes])
+                # Placeholder = protection contre les injections SQL type drop table caché dans une pseudo variable
+                placeholders = ", ".join([f":{col}" for col in colonnes])
                 insert_sql = text(f"""
-                    INSERT INTO {nom_de_la_table} ({colonnes_str}) 
-                    VALUES ({placeholder})
+                    INSERT INTO `{nom_de_la_table}` ({colonnes_str})
+                    VALUES ({placeholders});
                 """)
-                valeurs = {col: row[col] if pd.notna(row[col]) else None for col in colonnes} # gestion des NaN : ils deviennent None
-                with engine.begin() as conn: # engine.begin et pas engine.connnect car sinon il n'y a pas de transaction explicite(commit)
+                valeurs = {
+                    col: row[col] if pd.notna(row[col]) else None
+                    for col in colonnes
+                }
+                with engine.begin() as conn:
                     conn.execute(insert_sql, valeurs)
-                    print(f"ligne insérée avec {valeurs}")
+            print(f"Toutes les lignes du fichier `{csv}` ont été insérées avec succès dans la table `{nom_de_la_table}`.")
         except Exception as e:
-            print(f"Erreur : {e}")
+            print(f"Erreur lors de la création ou de l'insertion : {e}")
+            traceback.print_exc()
             return None
